a4b55601880edd9c3039dd8d76affdc4531f9f0e
Thomas <rockenbauer.thomas@gmail.com>
2016-06-10 05:35:14 +0200

    change code to FSM

--- Energy_Monitor.ino
+++ Energy_Monitor.ino
@@ -18,42 +18,6 @@ TCPClient client;
 
 CurrentMonitor current;
 
-//cloud variables
-double currentReading;
-double currentReading_ch2;
-double kwh;
-double kwh_ch2;
-int firmware;
-int maxCurrent;
-int numberOfChannels;
-int sensorType;
-double ACVoltageD;
-
-float temp, pres, hum = 0;
-
-unsigned long previousMillis = 0;
-unsigned long previousMillis2 = 0;
-unsigned long interval = 30000;
-
-//local variables
-float ACVoltage = 230.00;
-unsigned long upTime = 0;
-unsigned long lastReadTime;
-int acVoltageStorageIndex = 0;
-int kwhStorageStartIndex = 4;
-int tripLimitIndex = 12;
-double tripLimit = 200.00;
-double lastReading = 0;
-
-double wattage_ch1 = 0;
-double wattage_ch2 = 0;
-
-
-//Cloud Functions
-int setACVoltagte(String voltage);
-int clearKWH(String channel);
-int setTripLimit(String limit);
-
 bool sendInflux(String payload) {
     client.connect(INFLUXDB_HOST, INFLUXDB_PORT);
     if(client.connected())
@@ -72,82 +36,152 @@ bool sendInflux(String payload) {
     client.println(); 
     client.flush();
     client.stop();
-    
+    return true;
 }
+
+#define WAIT_CURRENT_TIME 2000 /* milliseconds */
+#define WAIT_PUBLISH_TIME 60000 /* milliseconds */
+
+typedef enum {
+    INITIALIZE_SENSORS,
+    WAIT_ONLINE,
+    ALIVE,
+    PUBLISH,
+    SET_TIMER,
+    WAIT_CURRENT,
+    WAIT_PUBLISH,
+    READ_CHANNEL_1,
+    READ_CHANNEL_2,
+    READ_SENSORS,
+    CREATE_STRING
+} FSM_state_t;
 
 void setup() {
-    Serial.begin(19200);
-    if(!current.initialize(0,0,0,0)){
-        Serial.println("Initialize failed");
-    }
-    firmware = current.firmwareVersion;
-    maxCurrent = current.maxCurrent;
-    numberOfChannels = current.numberOfChannels;
-    sensorType = current.sensorType;
-    ACVoltageD = (double)ACVoltage;
-    Serial.println(ACVoltageD);
-    getInfoFromStorage();
-    Particle.variable("Firmware", firmware);
-    Particle.variable("Max_Current", maxCurrent);
-    Particle.variable("Sensor_type", sensorType);
-    Particle.variable("Channels", numberOfChannels);
-    Particle.variable("Current", currentReading);
-    Particle.variable("KWH_Readings", kwh);
-    Particle.variable("ACVoltage", ACVoltageD);
-    Particle.function("SetACVoltage", setACVoltagte);
-    Particle.function("ClearKWHs", clearKWH);
-    Particle.function("Trip", setTripLimit);
-    
-    Serial.println(F("BME280 test"));
-
-    if (!bme.begin()) {
-        Serial.println("Could not find a valid BME280 sensor, check wiring!");
-    while (1);
-    }
-    delay(1000);
-    //kwh = readInflux();
     
 }
 
-void loop(){
-    if(current.deviceStatusReady){
-        while(current.deviceStatusReady){
+void loop()
+{
+    static FSM_state_t myState = WAIT_ONLINE;
+    static uint32_t saveTime;
+    static String data;
+    static float temp;
+    static float pres;
+    static float hum;
+    static double kwh_ch1;
+    static double kwh_ch2;
+    static unsigned long lastReadTime;
+    static double wattage_ch1;
+    static double wattage_ch2;
+    static unsigned long upTime;
+    static float ACVoltage = 230.00;
+    static double currentReading_ch1;
+    static double currentReading_ch2;
+    static double lastReading;
+
+    switch (myState) {
+        
+        case INITIALIZE_SENSORS:
+            if(!current.initialize(0,0,0,0)){
+                //Serial.println("Initialize failed");
+            }
+    
+
+            if (!bme.begin()) {
+                //Serial.println("Could not find a valid BME280 sensor, check wiring!");
+            while (1);
+            }
+            //Particle.publish("Sensors initialized");
+            myState = ALIVE;
+            break;
+    
+
+        case WAIT_ONLINE: // stay in this state  until we're connected to the big old cloud in the sky
+            if (Particle.connected()){
+                myState = INITIALIZE_SENSORS;
+            }
+            break;
+
+        case ALIVE: // we're alive! shout about it
+            Particle.publish("Came online at", String(millis()) );
+            myState = SET_TIMER; // next time through loop(), we'll set up our custom timer
+            break;
+
+        case PUBLISH: // it's been TIME_TO_WAIT milliseconds -- make some noise!
+            if ( !Particle.connected() ) { // first though ... did we get disconnected somehow?
+                myState = WAIT_ONLINE;
+            } else {
+                Particle.publish("PING!! at ", String(millis()) );
+                //Particle.publish("CH2", String::format("{\"KWh\":%.2f,\"W\":%.2f,\"Temp\":%.2f,\"Hum\":%.2f,\"Pres\":%.2f}", kwh_ch2, wattage_ch2, temp, hum, pres), 60, PRIVATE);
+                sendInflux(data);
+
+                myState = SET_TIMER;
+            }
+            break;
+
+        case SET_TIMER: // record the current time for the WAIT state to reference back to
+            saveTime = millis();
+            //Particle.publish("Timer set");
+            myState = WAIT_CURRENT;
+            break;
+
+        case WAIT_CURRENT: // stay in this state until TIME_TO_WAIT milliseconds have gone by since the SET_TIMER state
+            if ( millis() > (saveTime + WAIT_CURRENT_TIME) ) {
+                
+                // Time's up! But we'll drop out of loop() for and get it done next time through
+                // This allows all the background network stuff the best chance of keeping up with business
+                //Particle.publish("Ready to read current");
+                myState = READ_CHANNEL_1; 
+            }
+            break;
             
-            unsigned long currentMillis2 = millis();
-            if(currentMillis2 - previousMillis2 >= interval) {
+        case WAIT_PUBLISH: // stay in this state until TIME_TO_WAIT milliseconds have gone by since the SET_TIMER state
+            if ( millis() > (saveTime + WAIT_PUBLISH_TIME) ) {
+                
+                // Time's up! But we'll drop out of loop() for and get it done next time through
+                // This allows all the background network stuff the best chance of keeping up with business
+                myState = CREATE_STRING; 
+            } else {
+                myState = WAIT_CURRENT;
+            }
+            break;
             
-                //Read current on channel 1
+        case READ_CHANNEL_1: 
+            if( !current.deviceStatusReady){
+                Particle.publish("not ready");
+                myState = WAIT_CURRENT;
+            } else {
                 double c1 = current.readChannelCurrent(1);
                 if(c1 != current.failedCommand){
+                    //Particle.publish("hello 2");
                     //Publish most recent current reading
-                    currentReading = c1;
+                    currentReading_ch1 = c1;
            
-                    lastReading = currentReading;
+                    lastReading = currentReading_ch1;
                     // Calculate Kilowatt hours
                     // Calculate Wattage
-                    wattage_ch1 = currentReading*ACVoltage;
+                    double wattage_ch1 = currentReading_ch1*ACVoltage;
                     //Calculate hours
                     upTime = millis();
                 
-                    double hours = (upTime - lastReadTime) / (60.00 * 60.00 * 1000.00);
+                    double hours_ch1 = (upTime - lastReadTime) / (60.00 * 60.00 * 1000.00);
                 
                     //Calculate Kilowatt hours
-                    kwh = kwh + ((wattage_ch1 * hours)/1000);
-                    //Store kwh in eeprom so we do not loose it on power loss.
-                    //EEPROM.put(kwhStorageStartIndex, kwh);
-                
-                    /*unsigned long currentMillis = millis();
-               
-                    if(currentMillis - previousMillis >= interval) {
-                        bool sent = Particle.publish("CH1", String::format("{\"KWh\":%.2f,\"W\":%.2f}", kwh, wattage), 60, PRIVATE); 
-                
-                        previousMillis = currentMillis;
-                    } */
-                
+                    kwh_ch1 = kwh_ch1 + ((wattage_ch1 * hours_ch1)/1000);
+                    //Particle.publish(String(c1));
+                    myState = READ_CHANNEL_2;
+                    
                 } else{
-                    Serial.println("Reading of current channel 1 failed");
+                    Particle.publish("Reading of current channel 1 failed");
+                    myState = WAIT_CURRENT;
                 }
+            }
+            break;
             
+        case READ_CHANNEL_2:
+            if( !current.deviceStatusReady){
+                myState = WAIT_CURRENT;
+            } else {
                 double c2 = current.readChannelCurrent(2);
                 if(c2 != current.failedCommand){
                     //Publish most recent current reading
@@ -161,87 +195,28 @@ void loop(){
                     lastReadTime = millis();
                     //Calculate Kilowatt hours
                     kwh_ch2 = kwh_ch2 + ((wattage_ch2 * hours_ch2)/1000);
-                    //Store kwh in eeprom so we do not loose it on power loss.
-                    //EEPROM.put(kwhStorageStartIndex, kwh);
-                
+                    //Particle.publish(String(wattage_ch2));
+                    myState = READ_SENSORS;
                 }else{
-                    Serial.println("Reading of current channel 2 failed");
+                    Particle.publish("Reading of current channel 2 failed");
+                    myState = WAIT_CURRENT;
                 }
-                //We read current on the circuit once per second.
-             
-                temp = bme.readTemperature();
-                pres = bme.readPressure() / 100.0F;
-                hum = bme.readHumidity();
-            
-                bool sent = Particle.publish("CH2", String::format("{\"KWh\":%.2f,\"W\":%.2f,\"Temp\":%.2f,\"Hum\":%.2f,\"Pres\":%.2f}", kwh_ch2, wattage_ch2, temp, hum, pres), 60, PRIVATE);
-                String data = "sensors,Location=Bachgasse Temp=" + String(temp, 2) + ",Hum=" + String(hum, 2) + ",Pres=" + String(pres) + ",kWh=" + String(kwh_ch2, 2) + ",P=" + String(wattage_ch2);
-                //Serial.println(data);
-                sendInflux(data);
-                //readInflux();
-                previousMillis2 = currentMillis2;
-            }else{
-                return;
             }
-            
-        }
-    }else{
-        Serial.println("Device not ready");
-        Particle.publish("Device not ready");
-        delay(1000);
         
-    }
-}
-
-
-int setACVoltagte(String voltage){
-    ACVoltage = voltage.toFloat();
-    ACVoltageD = (double)ACVoltage;
-    EEPROM.put(acVoltageStorageIndex, ACVoltage);
-    return 1;
-}
-
-int clearKWH(String channelNumber){
-    kwh = 0.00;
-    EEPROM.put(kwhStorageStartIndex, kwh);
-    return 1;
-    
-}
-
-int setTripLimit(String limit){
-    float trip = limit.toFloat();
-    tripLimit = (double)trip;
-    EEPROM.put(tripLimitIndex, tripLimit);
-    
-}
-
-void getInfoFromStorage(){
-    float tACVoltage;
-    EEPROM.get(acVoltageStorageIndex, tACVoltage);
-    String emptyCheck = String(tACVoltage);
-    if(emptyCheck.equalsIgnoreCase("0.000000")){
-        Serial.println("No ACVoltage reading stored");
-    }else{
-        ACVoltage = tACVoltage;
-        ACVoltageD = (double)ACVoltage;
-    }
-
-    double tKWHReading;
-    EEPROM.get(kwhStorageStartIndex, tKWHReading);
-    String eCheck = String(tKWHReading);
-    if(eCheck.equalsIgnoreCase("0.000000")){
-        Serial.println("No Stored kWH readings for channel 1 \n");
-    }else{
-        Serial.printf("%.4f stored for Channel %i \n", tKWHReading, 1);
-        kwh = tKWHReading;
-    }
-    
-    double tTripLimit;
-    EEPROM.get(tripLimitIndex, tTripLimit);
-    String eCheck1 = String(tTripLimit);
-    if(eCheck1.equalsIgnoreCase("0.000000")){
-        Serial.println("No Stored trip limit for channel 1 \n");
-    }else{
-        Serial.printf("%.4f stored for Channel %i trip limit \n", tTripLimit, 1);
-        tripLimit = tTripLimit;
+        case READ_SENSORS:
+            temp = bme.readTemperature();
+            pres = bme.readPressure() / 100.0F;
+            hum = bme.readHumidity();
+            //Particle.publish(String(hum));
+            myState = WAIT_PUBLISH;
+            break;
+        
+        case CREATE_STRING:
+            data = "sensors,Location=Bachgasse Temp=" + String(temp, 2) + ",Hum=" + String(hum, 2) + ",Pres=" + String(pres) + ",kWh=" + String(kwh_ch2, 2) + ",P=" + String(wattage_ch2);
+            myState = PUBLISH;
+            break;
+        
+        default:
+            myState = WAIT_ONLINE;
     }
 }
